6a1a2bb5949d689e930b0360aea9bef98cbb72da
Yusei Nakamura <yusei2477@icloud.com>
2020-09-12 17:04:38 +0900

    試しコミット

---
+++ OriginalGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OriginalGame
+//
+//  Created by 中村優聖 on 2020/09/12.
+//  Copyright © 2020 com.litech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
